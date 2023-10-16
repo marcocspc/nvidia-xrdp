9fdfac4eba3c4f93d47f7836867533d443bcb7a0
Marco Araujo <maraujo@atlassian.com>
2023-10-16 14:43:47 -0300

    fix: added user creation to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+if ! [ -f /etc/user_created ] ; then
+    mkdir -p /home/$XRDPUSER
+    useradd --system \
+   --shell /bin/bash \
+   --gecos 'Default xrdp user.' \
+   --group \
+   --disabled-password \
+   --home /home/$XRDPUSER
+
+   touch /etc/user_created
+
+   echo "User $XRDPUSER created. Now run passwd to set it's password and then run the container again!"
+   exit 
+fi
+
 /usr/local/sbin/xrdp-sesman --nodaemon &
 
 exec /usr/local/sbin/xrdp --nodaemon 
