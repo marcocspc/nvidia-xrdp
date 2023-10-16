7307494cfa1942bfa0d9ac560863146dcf84a93d
Marco Araujo <maraujo@atlassian.com>
2023-10-16 14:57:44 -0300

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,9 +14,10 @@ if ! [ -f /etc/user_created ] ; then
    && touch /etc/user_created \
    \
    && echo "User $XRDPUSER created. Now run passwd to set it's password and then run the container again!"
-   exit 
 fi
 
+echo "Starting xrdp-sesman"
 /usr/local/sbin/xrdp-sesman --nodaemon &
 
+echo "Starting xrdp"
 exec /usr/local/sbin/xrdp --nodaemon 
