b99ed2273f6d469a902ec51b1982ff0b7f5eedd2
Marco Araujo <maraujo@atlassian.com>
2023-10-16 14:30:38 -0300

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/sbin/xrdp-sesman  >> /dev/sdtout 2>&1 &
+/usr/local/sbin/xrdp-sesman --no-daemon >> /dev/sdtout 2>&1 &
 
-exec /usr/local/sbin/xrdp  >> /dev/sdtout 2>&1
+exec /usr/local/sbin/xrdp --no-daemon >> /dev/sdtout 2>&1
